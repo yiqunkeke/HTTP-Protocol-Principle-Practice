5b83d8e61a0b1f756bf6460a6e67a2726c62f881
lijingke <1431806069@qq.com>
2019-09-25 15:45:13 +0800

    CORS-服务端设置headers、JSONP、预请求

--- code/cors/server.js
+++ code/cors/server.js
@@ -4,11 +4,19 @@ const fs = require('fs')
 http.createServer(function (request, response) {
   console.log('request come', request.url)
 
-  const html = fs.readFileSync('test.html', 'utf8')
+  const html = fs.readFileSync('test.html', 'utf8') // 1.在读取内容时，一定要设置为utf8格式，这样读取出来的是字符串。
+                                                    // 如果未设置utf8格式，则会读取为二进制格式，则无法使用node.js中的response.end()来发送了
   response.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
+    'Content-Type': 'text/html'   //2.这里需要设置'Content-type'这个头部为'text/html'。
+  })                              // node.js默认自动为我们加了这个Content-type为text/html，如果我们强制该为text/plain，则会是字符串
+  
+  // response.writeHead(200, {
+  //   'Content-Type': 'text/plain'
+  // })
+
   response.end(html)
 }).listen(8888)
 
 console.log('server listening on 8888')
+
+// 3. 所有的在8888服务中的ajax请求，都必须在8888这个域中，如果跨域，则必须服务器那边同意你跨域才可以正常显示返回的内容。
--- code/cors/server2.js
+++ code/cors/server2.js
@@ -2,14 +2,23 @@ const http = require('http')
 
 http.createServer(function (request, response) {
   console.log('request come', request.url)
-
+  
+  // 突破限制的几种方法
   response.writeHead(200, {
-    'Access-Control-Allow-Origin': 'http://127.0.0.1:8888',
-    'Access-Control-Allow-Headers': 'X-Test-Cors',
-    'Access-Control-Allow-Methods': 'POST, PUT, DELETE',
-    'Access-Control-Max-Age': '1000'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Test-Cors',  // 允许请求中有自定义的header
+    'Access-Control-Allow-Methods': 'POST, PUT, DELETE', // 默认跨域只允许GET POST HEAD方式的请求
+    'Access-Control-Max-Age': '1000'  // 1000s之内，不需要在发起预请求进行验证了，直接发正式的请求就可以。
   })
   response.end('123')
+  // 1. 如果不设置Access-Control-Allow-Origin头，实际上，http请求依然可以发送，并且内容也依然可以返回，
+  // 只不过浏览器在解析返回内容时，如果发现返回的响应中没有写Access-Control-Allow-Origin头的话，浏览器依据安全策略，会自动把返回的内容屏蔽掉。
+  // 即浏览器帮你把返回内容拦截掉了。
+  // 这其实是浏览器提供的功能
+
+  // 2. 如果使用curl命令发送跨域的http请求,则不存在跨域概念，即任何的http请求都可以发送，并且都可以返回内容，而且都可以正常输出的。
+
+  // 3. 所以这是 “浏览器” 的 “同域限制” 。
 }).listen(8887)
 
 console.log('server listening on 8887')
