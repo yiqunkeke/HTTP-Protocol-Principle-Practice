92323e5bd0944e776d99fd50f5cbc468a1cf4745
lijingke <1431806069@qq.com>
2019-10-10 11:20:21 +0800

    数据协商

--- code/accept/server.js
+++ code/accept/server.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
   const html = fs.readFileSync('test.html')
   response.writeHead(200, {
     'Content-Type': 'text/html',
-    // 'X-Content-Options': 'nosniff'
+    // 'X-Content-Options': 'nosniff'   // 设置之后，IE6就不会主动预测返回数据的格式了，了解即可
     'Content-Encoding': 'gzip'
   })
   response.end(zlib.gzipSync(html))
